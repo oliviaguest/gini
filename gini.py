884c599a2ce574c79adcd52d78eadea798e77ba1
Olivia Guest <guest.olivia@gmail.com>
2017-02-24 18:29:59 +0000

    just tidying, no code change

--- gini.py
+++ gini.py
@@ -2,13 +2,22 @@ import numpy as np
 
 def gini(array):
     """Calculate the Gini coefficient of a numpy array."""
-    # based on bottom eq: http://www.statsdirect.com/help/generatedimages/equations/equation154.svg
-    # from: http://www.statsdirect.com/help/default.htm#nonparametric_methods/gini.htm
-    array = array.flatten() #all values are treated equally, arrays must be 1d
+    # based on bottom eq:
+    # http://www.statsdirect.com/help/generatedimages/equations/equation154.svg
+    # from:
+    # http://www.statsdirect.com/help/default.htm#nonparametric_methods/gini.htm
+    # All values are treated equally, arrays must be 1d:
+    array = array.flatten()
     if np.amin(array) < 0:
-        array -= np.amin(array) #values cannot be negative
-    array += 0.0000001 #values cannot be 0
-    array = np.sort(array) #values must be sorted
-    index = np.arange(1,array.shape[0]+1) #index per array element
-    n = array.shape[0]#number of array elements
-    return ((np.sum((2 * index - n  - 1) * array)) / (n * np.sum(array))) #Gini coefficient
+        # Values cannot be negative:
+        array -= np.amin(array)
+    # Values cannot be 0:
+    array += 0.0000001
+    # Values must be sorted:
+    array = np.sort(array)
+    # Index per array element:
+    index = np.arange(1,array.shape[0]+1)
+    # Number of array elements:
+    n = array.shape[0]
+    # Gini coefficient:
+    return ((np.sum((2 * index - n  - 1) * array)) / (n * np.sum(array)))
